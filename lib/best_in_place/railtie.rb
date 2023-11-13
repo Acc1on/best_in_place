cb96c908232c1a2e7ab72ddfa5650319fc1f3be9
prajwal-5 <raiprajwal.paul@gmail.com>
2023-11-13 12:42:56 +0530

    Fixed number of arguments error while initializing
    Co-authored-by: Ernest S <514433+ernest-ns@users.noreply.github.com>

--- lib/best_in_place/railtie.rb
+++ lib/best_in_place/railtie.rb
@@ -3,8 +3,20 @@ require 'action_view/base'
 
 module BestInPlace
   class Railtie < ::Rails::Railtie #:nodoc:
+    def rails_version
+      Gem::Version.new(Rails::VERSION::STRING)
+    end
+
+    def rails_6_1?
+      Gem::Requirement.new('~> 6.1.0').satisfied_by?(rails_version)
+    end
+
     config.after_initialize do
-      BestInPlace::ViewHelpers = ActionView::Base.new
+      BestInPlace::ViewHelpers = if rails_6_1?
+                                    ActionView::Base.new(ActionView::LookupContext.new([]), {}, {})
+                                  else
+                                    ActionView::Base.new
+                                  end
     end
   end
 end
